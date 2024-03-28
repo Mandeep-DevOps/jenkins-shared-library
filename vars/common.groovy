77158ddf71933945d21bb17dd146300d91da69fe
SVR Kalluru <raghuk.vit@gmail.com>
2024-03-28 08:03:17 +0530

    Adding steps into library

---
+++ vars/common.groovy
@@ -0,0 +1,41 @@
+def unitTests() {
+  stage('Unit Tests') {
+    echo 'OK'
+  }
+}
+
+def integrationTests() {
+  stage('Integration Tests') {
+    echo 'OK'
+  }
+}
+
+def codeQuality() {
+  stage('Code Quality') {
+    echo 'OK'
+  }
+}
+
+def sast() {
+  stage('SAST') {
+    echo 'OK'
+  }
+}
+
+def sca() {
+  stage('SCA') {
+    echo 'OK'
+  }
+}
+
+def secretDetection() {
+  stage('SECRET Detection') {
+    echo 'OK'
+  }
+}
+
+def artifactProduce() {
+  stage('SECRET Detection') {
+    echo 'OK'
+  }
+}
