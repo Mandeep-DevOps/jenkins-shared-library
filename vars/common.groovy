dc43575df33015c7be8305ccabcefa4611ac6c66
SVR Kalluru <raghuk.vit@gmail.com>
2024-03-29 08:23:48 +0530

    Update steps

--- vars/common.groovy
+++ vars/common.groovy
@@ -43,8 +43,8 @@ def secretDetection() {
 def artifactProduce() {
   stage('Produce Artifact') {
     sh 'aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 739561048503.dkr.ecr.us-east-1.amazonaws.com'
-    sh 'docker build -t 739561048503.dkr.ecr.us-east-1.amazonaws.com/${TAG_NAME}:${} .'
-    sh 'docker push 739561048503.dkr.ecr.us-east-1.amazonaws.com/${TAG_NAME}:${}'
+    sh 'docker build -t 739561048503.dkr.ecr.us-east-1.amazonaws.com/${service_name}:${TAG_NAME} .'
+    sh 'docker push 739561048503.dkr.ecr.us-east-1.amazonaws.com/${service_name}:${TAG_NAME}'
   }
 }
 
